fd78a82c87d7f4e86eb72ea5700ae9a78430813c
Mantri Venkata Sai Nischal <sainischalmv@gmail.com>
2025-04-16 23:40:55 +0530

    doc details is also fixed

--- routes/docDetails.js
+++ routes/docDetails.js
@@ -1,7 +1,6 @@
 import express from "express";
 import jwt from "jsonwebtoken";
 import { User } from "../models/userModel.js";
-import mongoose from "mongoose";
 
 const router = express.Router();
 
