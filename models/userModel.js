8ee165c278473da0fa89a1e7586a0301f96bbdf5
Mantri Venkata Sai Nischal <sainischalmv@gmail.com>
2025-04-16 22:53:37 +0530

    new route for edititng doctor data is added

--- models/userModel.js
+++ models/userModel.js
@@ -2,12 +2,12 @@ import mongoose from "mongoose";
 
 // User Schema
 const userSchema = new mongoose.Schema({
-    patients: [{ type: mongoose.Schema.Types.ObjectId, ref: "Patient" }],
+    patients: [{ type: mongoose.Schema.Types.ObjectId, ref: "Patient", default: [] }],
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    name: { type: String, },
-    phone: { type: String, },
-    specialization: { type: String },
+    name: { type: String, default: "" },
+    phone: { type: String, default: "" },
+    specialization: { type: String, default: "" },
 }, { timestamps: true });
 
 export const User = mongoose.model("User", userSchema);
---
+++ routes/docDetails.js
@@ -0,0 +1,49 @@
+import express from "express";
+import jwt from "jsonwebtoken";
+import { User } from "../models/userModel.js";
+import mongoose from "mongoose";
+
+const router = express.Router();
+
+// Middleware to verify JWT token
+const verifyToken = (req, res, next) => {
+    const bearerHeader = req.headers["authorization"];
+    if (!bearerHeader || !bearerHeader.startsWith("Bearer ")) {
+        return res.status(401).json({ message: "Invalid or missing token." });
+    }
+    const token = bearerHeader.split(" ")[1];
+
+    if (!token) {
+        return res.status(401).json({ message: "Access denied. No token provided." });
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.SECRET);
+        req.user = decoded; // Store the decoded token in the request object
+        next();
+    } catch (error) {
+        res.status(400).json({ message: "Invalid token." });
+    }
+};
+// Send Message on Behalf of Doctor and Close the Patient Record
+router.post("/", verifyToken, async (req, res) => {
+    const { id: doctorId } = req.user;
+    const { name, phone, specialization } = req.body;
+    const doctor = await User.findById(doctorId);
+    if (!doctor) {
+        return res.status(404).json({ message: "Doctor not found" });
+    }
+    //i need to add name ,phone , specialization, to users
+
+    const updatedDoctor = await User.findByIdAndUpdate(doctorId, {
+        name: name,
+        phone: phone,
+        specialization: specialization,
+    }, { new: true });
+    if (!updatedDoctor) {
+        return res.status(404).json({ message: "Doctor not found" });
+    }
+
+    res.status(200).json({ message: "Doctor details updated successfully", doctor: updatedDoctor });
+});
+export default router;
