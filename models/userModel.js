8d6e417ed5c8a0b2adc981c2ed20f17d6780fb7c
Mantri Venkata Sai Nischal <sainischalmv@gmail.com>
2025-04-16 20:08:41 +0530

    doctor response is set and usermodel is udpated with patients aaray

--- models/userModel.js
+++ models/userModel.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 // User Schema
 const userSchema = new mongoose.Schema({
+    patients: [{ type: mongoose.Schema.Types.ObjectId, ref: "Patient" }],
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     name: { type: String, },
