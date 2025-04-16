dacb47e5a74a204a4b7804310fbe628928048764
Mantri Venkata Sai Nischal <sainischalmv@gmail.com>
2025-04-17 04:12:07 +0530

    updated the record route , with doctor name

--- routes/patientRecord.js
+++ routes/patientRecord.js
@@ -46,7 +46,7 @@ router.get("/", verifyToken, async (req, res) => {
         // Find records using doctor id in patientRecord
         const patientRecords = await PatientRecord.find({ doctor: doctorId, isClosed: false });
         if (!patientRecords || patientRecords.length === 0) {
-            return res.status(200).json({ patientRecords: [], recordDetails: [] });
+            return res.status(200).json({ patientRecords: [], recordDetails: [], doctorName: doctor.name });
         }
 
         // Extract record IDs and respective patient IDs
@@ -75,7 +75,7 @@ router.get("/", verifyToken, async (req, res) => {
             })
         );
 
-        res.status(200).json({ patientRecords: populatedRecords, recordDetails, doctor });
+        res.status(200).json({ patientRecords: populatedRecords, recordDetails, doctorName: doctor.name });
     } catch (error) {
         console.error("🚨 Error fetching patient records:", error.message);
         res.status(500).json({ message: "Internal server error", error: error.message });
